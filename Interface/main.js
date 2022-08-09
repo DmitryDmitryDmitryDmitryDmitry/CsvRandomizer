e74de70d78b6e489c6f0b2e27ce1bafbae65ba1c
DmitryDmitryDmitryDmitryDmitry <dubovskiy-1997@mail.ru>
2022-08-09 23:02:25 +0300

    Create main.js

---
+++ Interface/main.js
@@ -0,0 +1,12 @@
+ window.onload = () => {
+
+	const uploadFile = document.getElementById("upload-file");
+	const uploadBtn = document.getElementById("upload-btn");
+	const uploadText = document.getElementById("upload-text");
+
+	uploadBtn.addEventListener("click", function () {
+	  uploadFile.click();
+	});
+	
+	  
+}
